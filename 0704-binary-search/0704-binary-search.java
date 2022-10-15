a95f9ff6277daef5fd517544be97b9e80cf74564
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-10-15 11:10:20 +0530

    Time: 0 ms (100.00%), Space: 54.6 MB (22.47%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,8 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        //  TIME COMPLEXITY OF THE FOLLOWING CODE IS:- O(n)
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+        
+        // NOW USING BINARY SEARCH WE ARE DECREASING THE TIME COMPLEXITY
+        // NOW THE TIME COMPLEXITY DECRESED TO:- O(log n)
+        int n=nums.length;
+        int l=0, r=n-1, mid;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(nums[mid]<target){
+                l=mid+1;            
+            }
+            if(nums[mid]>target){
+                r=mid-1;
+            }
+            if(nums[mid]==target){
+                return mid;
             }
         }
         return -1;
