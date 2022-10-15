f9c8416ded7d11c4e1b2c92e414bdb10eb07dd30
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-10-15 23:09:14 +0530

    Added solution - LeetHub

---
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG{
+	public static void main(String args[]) throws IOException { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t > 0){
+       		int rows=sc.nextInt();
+       		int columns=sc.nextInt();
+			
+			int matrix[][]=new int[rows][columns];
+          
+        	for(int i=0; i<rows;i++){            
+            	for(int j=0; j<columns;j++){
+                	matrix[i][j]=sc.nextInt();
+            	}
+         	}
+         	int target = sc.nextInt();
+         	
+			Solution x = new Solution();
+			
+			if (x.search(matrix, rows, columns, target)) 
+				System.out.println(1); 
+			else
+				System.out.println(0); 
+			t--;
+		}
+	} 
+}
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{ 
+    //Function to search a given number in row-column sorted matrix.
+	static boolean search(int mat[][], int n, int m, int x) 
+	{  
+	    // code here 
+	    int i=0, j=m-1;
+	    while(i<n && j>=0){
+	        if(mat[i][j]<x){
+	            i++;
+	        }
+	        else if(mat[i][j]>x){
+	            j--;
+	        }
+	        else
+	        return true;
+	    }
+	    return false;
+	} 
+} 
