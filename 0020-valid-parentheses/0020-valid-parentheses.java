e4ead344f33ca8be59e127a7a4db2da00e2049c7
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-11-01 18:40:00 +0530

    Time: 3 ms (78.51%), Space: 41.9 MB (57.39%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> st= new Stack<>();
+    
+        Stack<Character> st= new Stack<>();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
                 st.push(s.charAt(i));
@@ -14,23 +15,57 @@ class Solution {
                 else{
                     if(st.empty())
                         return true;
-                    else
-                        return false;
+                    return false;
                 }
             }
         }
         if(st.empty())
             return true;
         return false;
+        //     Stack <Character> st= new Stack<>();
+    //     for(int i=0;i<s.length();i++){
+    //         if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+    //             st.push(s.charAt(i));
+    //         }
+    //         else{
+    //             if(st.empty())
+    //                 return false;
+    //             else if(match(st.peek(), s.charAt(i))){
+    //                 st.pop();
+    //             }
+    //             else{
+    //                 if(st.empty())
+    //                     return true;
+    //                 else
+    //                     return false;
+    //             }
+    //         }
+    //     }
+    //     if(st.empty())
+    //         return true;
+    //     return false;
+    // }
+    // static boolean match(char open, char close){
+    //     if(open=='(' && close==')')
+    //         return true;
+    //     else if(open=='[' && close==']')
+    //         return true;
+    //     else if(open=='{' && close=='}')
+    //         return true;
+    //     else 
+    //         return false;
+        
+        
+        
     }
-    static boolean match(char open, char close){
-        if(open=='(' && close==')')
-            return true;
-        else if(open=='[' && close==']')
-            return true;
-        else if(open=='{' && close=='}')
-            return true;
-        else 
-            return false;
-    }
+    static boolean match(char peek, char check){
+            if(peek=='(' && check==')')
+                return true;
+            else if(peek=='{' && check=='}')
+                return true;
+            else if(peek=='[' && check==']')
+                return true;
+            else return false;
+            
+        }
 }
