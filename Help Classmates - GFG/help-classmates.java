68cfe1c621af2a2754ae65a6a28fd4e55790e275
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-11-04 11:29:55 +0530

    Added solution - LeetHub

---
+++ Help Classmates - GFG/help-classmates.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*; 
+import java.io.*; 
+
+class GFG 
+{ 
+	public static void main (String[] args) { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            int array[] = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+
+            int ans[] = ob.help_classmate(array,n);
+
+           	for (int i=0; i<n; i++) 
+                System.out.print(ans[i]+" "); 
+            System.out.println();
+            t--;
+        }
+	} 
+} 
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+	public static int[] help_classmate(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    int []ans= new int[n];
+	    //ans[n-1]=-1;
+	    Stack<Integer> st= new Stack<>();
+	    for(int i=n-1;i>=0;i--){
+	        while(!st.empty() && st.peek()>=arr[i]) st.pop();
+	        if(!st.empty()) ans[i]=st.peek();
+	        else ans[i]=-1;
+	        st.push(arr[i]);
+	    }
+	    return ans;
+	} 
+}
