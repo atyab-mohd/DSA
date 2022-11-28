8c6b3197ad185b447597f8bd211c099459122b97
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-11-28 18:47:47 +0530

    Added solution - LeetHub

--- First Come First Serve - GFG/first-come-first-serve.java
+++ First Come First Serve - GFG/first-come-first-serve.java
@@ -21,10 +21,7 @@ class Solution {
         HashMap<Integer, Integer> hm= new HashMap<>();
         for(int i=0;i<n;i++) hm.put(arr[i], hm.getOrDefault(arr[i],0)+1);
         for(int i=0;i<n;i++){
-            if(hm.get(arr[i])==k){
-                return arr[i];
-                //break;
-            }
+            if(hm.get(arr[i])==k) return arr[i];
         }
         return -1;
     }
