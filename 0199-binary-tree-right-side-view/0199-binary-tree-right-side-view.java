9f3d1de57c22109bf5992d351993ea46470774f9
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-11-28 19:20:28 +0530

    Time: 2 ms (53.15%), Space: 42.5 MB (64.06%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,14 +15,30 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        // List<Integer> ans= new ArrayList<>();
+        // rightView(root, ans, 0);
+        // return ans;
+
+        //2. Solving by Level Order Transversal
         List<Integer> ans= new ArrayList<>();
-        rightView(root, ans, 0);
+        if(root == null) return ans;
+        Queue<TreeNode> q= new LinkedList<>();
+        q.add(root);
+        while(q.size()>0){
+            int size= q.size();
+            for(int i=0;i<size;i++){
+                TreeNode cur= q.poll();
+                if(i==0) ans.add(cur.val);
+                if(cur.right != null) q.add(cur.right);
+                if(cur.left != null) q.add(cur.left);
+            }
+        }
         return ans;
     }
-    void rightView(TreeNode root, List<Integer> ans, int level){
-        if(root == null) return;
-        if(level == ans.size()) ans.add(root.val); //add the value when the size of list equal to the level
-        rightView(root.right, ans, level+1);
-        rightView(root.left, ans, level+1);
-    }
+    // void rightView(TreeNode root, List<Integer> ans, int level){
+    //     if(root == null) return;
+    //     if(level == ans.size()) ans.add(root.val); //add the value when the size of list equal to the level
+    //     rightView(root.right, ans, level+1);
+    //     rightView(root.left, ans, level+1);
+    // }
 }
