3bbf34167e919d4b7fa21edaf39080e79b89eae8
simp-101 <115527333+simp-101@users.noreply.github.com>
2022-12-07 10:19:27 +0530

    Added solution - LeetHub

---
+++ Maximum Node Level - GFG/maximum-node-level.java
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+			    int ans = g.maxNodeLevel(root);
+			    System.out.println(ans);
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+	int data;
+	Node left,right;
+	
+	Node(int item)
+	{
+	    data = item;
+	    left = right = null;
+	}
+}
+*/
+class Tree
+{
+    // Return the level (0-indexed) with maximum number of nodes.
+    public static int maxNodeLevel(Node root)
+    {
+        // add your code here
+       int maxNodes = 0;
+       int ans = -1;
+       int level = 0;
+       if(root == null) return -1;
+       Queue<Node> q = new LinkedList<>();
+       q.add(root);
+       while(q.size() > 0){
+           int size = q.size();
+           ArrayList<Integer> nodes = new ArrayList<>();
+           for(int i=0;i<size;i++){
+               Node cur = q.poll();
+               nodes.add(cur.data);
+               if(cur.left != null) q.add(cur.left);
+               if(cur.right != null) q.add(cur.right);
+           }
+           
+           if(maxNodes < nodes.size()){
+               maxNodes = nodes.size();
+               ans = level;
+           }
+           level++;
+           //maxNodes = Math.max(maxNodes, nodes.size());
+       }
+       return ans;
+    }
+}
