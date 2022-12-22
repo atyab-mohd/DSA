05a2e93fac04a9ae1ac00ab0b30749f7f1ae0bd4
Atyab <115527333+simp-101@users.noreply.github.com>
2022-12-23 00:49:26 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (81.64%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,20 +1,30 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        if(n == 1) return 0;
-        if(n == 2){
-            if(nums[0] > nums[1]) return 0;
-            else return 1;
+        // int n = nums.length;
+        // if(n == 1) return 0;
+        // if(n == 2){
+        //     if(nums[0] > nums[1]) return 0;
+        //     else return 1;
+        // }
+        // int l=1, r=n-2;
+        // if(nums[0] > nums[l]) return 0;
+        // if(nums[n-1] > nums[r]) return n-1;
+        // while(l<=r){
+        //     int mid = (l+r)/2;
+        //     if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
+        //     else if(nums[mid] < nums[mid+1]) l = mid+1;
+        //     else r = mid-1;
+        // }
+        // return -1;
+        int l = 0;
+        int r = nums.length-1;
+        while(l<r){
+            int m = l + (r-l)/2;
+            if(nums[m]<nums[m+1])
+                l = m+1;
+            else
+                r = m;
         }
-        int l=1, r=n-2;
-        if(nums[0] > nums[l]) return 0;
-        if(nums[n-1] > nums[r]) return n-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]) return mid;
-            else if(nums[mid] < nums[mid+1]) l = mid+1;
-            else r = mid-1;
-        }
-        return -1;
+        return r; // when l=r
     }
 }
